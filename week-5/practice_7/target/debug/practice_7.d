a7dcc70c25810b51e7e70985daf93a69f244df3d
legitraph <raphaelaghachi@gmail.com>
2023-11-12 11:50:25 -0800

    (fifth)

---
+++ week-5/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\PC\Documents\a.ebubeCOS101\week-5\practice_7\target\debug\practice_7.exe: C:\Users\PC\Documents\a.ebubeCOS101\week-5\practice_7\src\main.rs
---
+++ week-5/project_3/target/debug/project_3.d
@@ -0,0 +1,1 @@
+C:\Users\PC\Documents\a.ebubeCOS101\week-5\project_3\target\debug\project_3.exe: C:\Users\PC\Documents\a.ebubeCOS101\week-5\project_3\src\main.rs
